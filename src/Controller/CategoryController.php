37777a284019402ad48a9b2e70d26d1266e321d8
Florent-F-Dev <68734060+Florent-F-Dev@users.noreply.github.com>
2021-05-16 22:09:54 +0200

    Initiation au composant form

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use ContainerGIOFUBA\getCategoryTypeService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category($slug, CategoryRepository $categoryRepository): Response
+    {
+
+        $category = $categoryRepository->findOneBy([
+            'slug' => $slug
+        ]);
+        if (!$category) {
+            throw $this->createNotFoundException("La catégorie demandée n'existe pas");
+        }
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * @Route("/admin/category/create", name="category_create")
+     */
+    public function create(Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+
+
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('category/create.html.twig', [
+            'formView' => $formView
+        ]);
+    }
+
+    /**
+     * @Route("/admin/category/{id}/edit", name="category_edit")
+     */
+    public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em, SluggerInterface $slugger)
+    {
+        $category = $categoryRepository->find($id);
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'formView' => $formView
+        ]);
+    }
+}
